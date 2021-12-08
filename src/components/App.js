653eb245c2794f4baec2cd5265d5199c374c08cb
Michael Orvin <morvin1212@gmail.com>
2021-12-07 20:44:49 -0700

    finished MVP

--- src/components/App.js
+++ src/components/App.js
@@ -48,4 +48,11 @@ const App = props => {
   );
 };
 
-export default App;
+const mapStateToProps = (state) => {
+  return{
+    displayFavorites: state.favoritesReducer.displayFavorites
+  }
+  
+}
+
+export default connect(mapStateToProps)(App);
